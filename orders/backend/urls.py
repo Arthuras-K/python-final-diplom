60e9f9aa6ef8a5b6334bba9ad2f77574a2295d4a
Arthuras <a-r-ch@yandex.ru>
2023-07-03 11:12:02 +0300

    added serializers and tasks

--- orders/backend/urls.py
+++ orders/backend/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 
-from .views import PartnerUpdate
+from .views import PartnerUpdate, ProductInfoViewSet, BasketView, OrderView
+
 
 app_name = 'backend'
 
-router = routers.SimpleRouter()
+router = DefaultRouter()
+router.register(r'product', ProductInfoViewSet, basename='products')
 
 urlpatterns = [
+    # Путь для обновления прайса от поставщика
     path('partner/update', PartnerUpdate.as_view(), name='partner-update'),
-    path('', include(router.urls)),
+    # Путь для просмотра корзины
+    path('basket', BasketView.as_view(), name='basket'),
+    # Путь для просмотра заказа
+    path('order', OrderView.as_view(), name='order'),
 ] + router.urls
 
